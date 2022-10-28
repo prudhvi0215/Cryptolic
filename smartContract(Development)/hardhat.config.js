f073890c4517db99584ddd71f452c98f15adc02e
Prudhvi Raj Valapolu <prudhviraj3099@gmail.com>
2022-10-28 13:50:42 +0530

    Updated-ApiSettings

--- smartContract(Development)/hardhat.config.js
+++ smartContract(Development)/hardhat.config.js
@@ -1,14 +1,14 @@
-
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 module.exports = {
-  solidity: '0.8.0',
+  solidity: "0.8.0",
   networks: {
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${process.env.HARDHAT_KEY}`,
-      accounts: ['77142206c05f7a06fe8d6decc1dc58ee9bb8d4fc50ef4dc4e1a48d2c81be4a7b']
-    }
-  }
-}
-
+      accounts: [
+        "77142206c05f7a06fe8d6decc1dc58ee9bb8d4fc50ef4dc4e1a48d2c81be4a7b",
+      ],
+    },
+  },
+};
--- smart_contract(Testing)/hardhat.config.js
+++ smart_contract(Testing)/hardhat.config.js
@@ -1,16 +1,16 @@
-
 // https://eth-goerli.alchemyapi.io/v2/Gx2KbRE8awd2QHW6bBoGbFjA41LLo5MF
 
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 module.exports = {
-  solidity: '0.8.0',
+  solidity: "0.8.0",
   networks: {
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${process.env.HARDHAT_KEY}`,
-      accounts: ['77142206c05f7a06fe8d6decc1dc58ee9bb8d4fc50ef4dc4e1a48d2c81be4a7b']
-    }
-  }
-}
-
+      accounts: [
+        "77142206c05f7a06fe8d6decc1dc58ee9bb8d4fc50ef4dc4e1a48d2c81be4a7b",
+      ],
+    },
+  },
+};
